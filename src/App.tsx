809d452db4c6cbc9c7502d1947fd7cf248b32249
KieranMueller <kieran98mueller@gmail.com>
2025-07-16 10:34:37 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, type ChangeEvent } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 type Status = null | 'Uploading...' | 'Success' | 'Error' | 'Awaiting Upload'
+type Advice = {
+    'css-class': string
+    content: string
+}
 type BackendResponse = {
     max_knee_angle: number
     average_largest_knee_angles: number
     frames_analyzed: number
     image_base64: string
-    video_base64: string
+    video_filename: string
+    advice: Advice
 }
 
 function App() {
     const [file, setFile] = useState<File | null>(null)
     const [status, setStatus] = useState<Status>(null)
-    const [testContent, setTestContent] = useState<any>()
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [response, setResponse] = useState<BackendResponse>()
     const [leg, setLeg] = useState<'left' | 'right' | null>(null)
     const [orientation, setOrientation] = useState<
         'portrait' | 'landscape' | null
     >(null)
+    const [videoUrl, setVideoUrl] = useState<string | null>(null)
 
     const handleFileSelect = async (event: ChangeEvent<HTMLInputElement>) => {
         const uploadedFile = event.target.files![0]
@@ -44,11 +52,32 @@ function App() {
                 )
                 const result = await response.json()
                 if (result) {
-                    console.log(result)
                     result.image_base64 = `data:image/jpeg;base64,${result.image_base64}`
-                    result.video_base64 = `data:video/mp4;base64,${result.video_base64}`
-                    setResponse(result)
+                    const advice: Advice = {
+                        'css-class':
+                            result.average_largest_knee_angles <= 150 &&
+                            result.average_largest_knee_angles >= 140
+                                ? 'green'
+                                : (result.average_largest_knee_angles < 140 &&
+                                      result.average_largest_knee_angles >=
+                                          135) ||
+                                  (result.average_largest_knee_angles > 150 &&
+                                      result.average_largest_knee_angles <= 155)
+                                ? 'orange'
+                                : 'red',
+                        content:
+                            result.average_largest_knee_angles <= 150 &&
+                            result.average_largest_knee_angles >= 140
+                                ? 'Within range'
+                                : result.average_largest_knee_angles < 140
+                                ? 'Raise saddle'
+                                : result.average_largest_knee_angles > 150
+                                ? 'Lower saddle'
+                                : '',
+                    }
+                    setResponse({ ...result, advice })
                     setStatus('Success')
+                    getDebugVideo(result.video_filename)
                 }
             } catch (e) {
                 console.log('Upload failed...', e)
@@ -57,152 +86,214 @@ function App() {
         }
     }
 
-    const testGet = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:8000/test')
-            const result = await response.json()
-            if (result) {
-                setTestContent(result.message)
-            }
-        } catch (e) {
-            console.log('error fetching test get', e)
-            setTestContent('Error connecting to backend...')
-        }
-    }
-
-    const test = () => {
-        testGet()
-        console.log(leg)
+    const getDebugVideo = async (filePath: string) => {
+        await fetch(`http://localhost:8000/debug-video/${filePath}`)
+            .then(response => response.blob())
+            .then(videoBlob => {
+                const videoUrl = URL.createObjectURL(videoBlob)
+                setVideoUrl(videoUrl)
+            })
+            .catch(e => {
+                console.log('error fetching debug video', e)
+            })
     }
 
     return (
-        <div>
-            <div>
-                <h2>Tips</h2>
-                <ul>
-                    <h3>Camera Positioning</h3>
-                    <li>Frame: Include entire bike and body</li>
-                    <li>Height: mid-thigh height </li>
-                    <li>Horizontal angle: camera perpendicular to bike</li>
-                    <li>Vertical angle: camera/lense level with floor</li>
-                    <li>Distance: 6-10ft</li>
-                    <h3>Video</h3>
-                    <li>
-                        Only include footage pedaling as normal (cut out video
-                        of getting on, and off the bike before uploading)
-                    </li>
-                    <li>
-                        No need to pedal slow, pedal as you would normally ride
-                    </li>
-                    <li>Higher fps = better results</li>
-                    <li>5-15s total duration</li>
-                    <h3>Other</h3>
-                    <li>Wear your usual cycling shoes and pants/bibs</li>
-                    <li>Bright lighting</li>
-                    <li>
-                        Select correct leg (upload footage of left and right to
-                        check for inbalances)
-                    </li>
-                    <h3>Optional</h3>
-                    <li>Light, solid color background</li>
-                    <li>Solid color shorts/pants</li>
-                </ul>
-                <h2>Results</h2>
-                <ul>
-                    <li>
-                        Optimal: 140-150° max knee angle, at bottom (6 o'clock)
-                        of pedal stroke
-                    </li>
-                </ul>
-            </div>
-            <div className='border'>
-                <div>
-                    <h3>Select Leg</h3>
-                    <label htmlFor='left'>Left</label>
-                    <input
-                        type='radio'
-                        id='left'
-                        name='leg'
-                        onChange={() => setLeg('left')}
-                    />
-                    <label htmlFor='right'>Right</label>
-                    <input
-                        type='radio'
-                        id='right'
-                        name='leg'
-                        onChange={() => setLeg('right')}
-                    />
+        <>
+            <Header />
+            <div className='container'>
+                <div className='top-content'>
+                    <div className='tips-div'>
+                        <h2>Instructions</h2>
+                        <ul>
+                            <h4>Camera Positioning</h4>
+                            <li>Frame: include entire bike and body</li>
+                            <li>Height: mid-thigh height </li>
+                            <li>
+                                Left/Right: line up camera with bottom bracket
+                            </li>
+                            <li>
+                                Horizontal angle: camera perpendicular to bike
+                            </li>
+                            <li>
+                                Distance: 6-10ft (any closer will yield poor
+                                results)
+                            </li>
+                            <h4>Video</h4>
+                            <li>Avoid 0.5/wide angle lens</li>
+                            <li>
+                                <strong>
+                                    Only include footage pedaling as normal (cut
+                                    out video of mounting/dismounting before
+                                    uploading){' '}
+                                </strong>
+                                <a
+                                    target='_blank'
+                                    href='https://www.google.com/search?q=trim+video+online+free&rlz=1C1CHBF_enUS1027US1027&oq=trim+video+online+free&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDINCAcQABiGAxiABBiKBTINCAgQABiGAxiABBiKBTIKCAkQABiABBiiBNIBCDI5OTlqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8'>
+                                    Video Trimmer
+                                </a>
+                            </li>
+                            <li>Pedal as you would normally ride</li>
+                            <li>Higher fps = better results</li>
+                            <li>5-15s total duration (10+ pedal rotations)</li>
+                            <h4>Other</h4>
+                            <li>
+                                Ensure bike is level (measure from the ground
+                                both front, and rear axles)
+                            </li>
+                            <li>
+                                Warmup for 15m prior (can result in up to 5° of
+                                error otherwise!)
+                            </li>
+                            <li>Wear your usual cycling shoes, pants/bibs</li>
+                            <li>Bright lighting</li>
+                            <li>
+                                Measure left and right knee Angles (check for
+                                imbalance)
+                            </li>
+                            <h3>Results</h3>
+                            <li>
+                                Optimal: 140-150° max knee angle, at bottom (6
+                                o'clock) of pedal stroke
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='upload-div'>
+                        <div>
+                            <h3>Select Leg</h3>
+                            <label htmlFor='left'>Left</label>
+                            <input
+                                type='radio'
+                                id='left'
+                                name='leg'
+                                onChange={() => setLeg('left')}
+                            />
+                            <label htmlFor='right'>Right</label>
+                            <input
+                                type='radio'
+                                id='right'
+                                name='leg'
+                                onChange={() => setLeg('right')}
+                            />
+                        </div>
+                        <div>
+                            <h3>Select Video Orientation</h3>
+                            <label htmlFor='portrait'>Portrait</label>
+                            <input
+                                type='radio'
+                                id='portrait'
+                                value='portrait'
+                                name='orientation'
+                                onChange={() => setOrientation('portrait')}
+                            />
+                            <label htmlFor='landscape'>Landscape</label>
+                            <input
+                                type='radio'
+                                id='landscape'
+                                value='landscape'
+                                name='orientation'
+                                onChange={() => setOrientation('landscape')}
+                            />
+                        </div>
+                        <div className='upload-elements'>
+                            {status !== 'Uploading...' && (
+                                <input
+                                    className='upload-input'
+                                    type='file'
+                                    onChange={handleFileSelect}
+                                />
+                            )}
+                            <button
+                                disabled={
+                                    !orientation ||
+                                    !file ||
+                                    !leg ||
+                                    status === 'Uploading...'
+                                }
+                                onClick={handleFileUpload}>
+                                Upload
+                            </button>
+                        </div>
+                        {file ? (
+                            <div className='file-info-div'>
+                                {status !== 'Uploading...' ? (
+                                    <h3>Status: {status}</h3>
+                                ) : (
+                                    <div className='spinner'></div>
+                                )}
+                                <p>File Name: {file.name}</p>
+                                <p>
+                                    File Size: {(file.size / 1024).toFixed(2)}{' '}
+                                    KB
+                                </p>
+                                <p>File Type: {file.type}</p>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    <h3>Select Video Orientation</h3>
-                    <label htmlFor='portrait'>Portrait</label>
-                    <input
-                        type='radio'
-                        id='portrait'
-                        value='portrait'
-                        name='orientation'
-                        onChange={() => setOrientation('portrait')}
-                    />
-                    <label htmlFor='landscape'>Landscape</label>
-                    <input
-                        type='radio'
-                        id='landscape'
-                        value='landscape'
-                        name='orientation'
-                        onChange={() => setOrientation('landscape')}
-                    />
-                </div>
-                <button onClick={test}>test</button>
-                {status !== 'Uploading...' && (
-                    <input
-                        className='upload-input'
-                        type='file'
-                        onChange={handleFileSelect}
-                    />
-                )}
-                <button
-                    disabled={
-                        !orientation ||
-                        !file ||
-                        !leg ||
-                        status === 'Uploading...'
-                    }
-                    onClick={handleFileUpload}>
-                    Upload
-                </button>
-            </div>
-            {file ? (
-                <div className='border'>
-                    <h1>Status: {status}</h1>
-                    <p>File Name: {file.name}</p>
-                    <p>File Size: {(file.size / 1024).toFixed(2)} KB</p>
-                    <p>File Type: {file.type}</p>
-                </div>
-            ) : (
-                <></>
-            )}
-            <p>{testContent}</p>
-            {response && (
-                <div>
-                    <h3>
-                        Max Knee Angle: {response.max_knee_angle.toFixed(1)}°
-                    </h3>
-                    <h3>
-                        Average 6 o'clock knee angle:{' '}
-                        {response.average_largest_knee_angles.toFixed(1)}°
-                    </h3>
-                    <p>(Calculated by averaging your 5 largest knee angles)</p>
-                    <p>Frames Analyzed: {response.frames_analyzed}</p>
 
-                    <img
-                        src={response.image_base64}
-                        className='result-img'
-                        alt='Max Angle Frame'
-                        style={{ maxWidth: '100%', marginBottom: '1rem' }}
-                    />
-                </div>
-            )}
-        </div>
+                {response && (
+                    <div className='results'>
+                        <div className='result-data'>
+                            <h3>
+                                Max Knee Angle:
+                                <span className={response.advice['css-class']}>
+                                    {' '}
+                                    {response.max_knee_angle.toFixed(1)}°
+                                </span>
+                            </h3>
+                            <h3>
+                                Average 6 o'clock knee angle:{' '}
+                                <span className={response.advice['css-class']}>
+                                    {response.average_largest_knee_angles.toFixed(
+                                        1
+                                    )}
+                                    °
+                                </span>
+                            </h3>
+                            <h3 className={response.advice['css-class']}>
+                                {response.advice.content}
+                            </h3>
+                            <p>
+                                (Calculated by averaging your 5 largest knee
+                                angles)
+                            </p>
+                            <p>Frames Analyzed: {response.frames_analyzed}</p>
+                            {videoUrl && (
+                                <div>
+                                    <button className='download-video-btn'>
+                                        <a
+                                            href={videoUrl}
+                                            download={
+                                                file?.name.substring(
+                                                    0,
+                                                    file?.name.lastIndexOf('.')
+                                                ) + '_analysis.mp4'
+                                            }>
+                                            Download Video Analysis
+                                        </a>
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                        <div className='result-media'>
+                            <img
+                                src={response.image_base64}
+                                className='result-img'
+                                alt='Max Angle Frame'
+                                style={{
+                                    maxWidth: '100%',
+                                    marginBottom: '1rem',
+                                }}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </>
     )
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import cyclist from '../assets/cyclist.svg'
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <img src={cyclist} alt='cyclist image' />
+        </div>
+    )
+}
